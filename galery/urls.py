63f805db0f3e310445a0b547e327df25cb528fda
LeoNarciso2022 <leoonarciso@gmail.com>
2023-01-18 11:48:18 -0300

    Segunda parte Final Frontier

---
+++ galery/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from galery.views import index
+
+
+
+urlpatterns = [
+    path('', index)
+]
+
--- setup/settings.py
+++ setup/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path, os
-from dotenv import load_dotenv
+from  dotenv import load_dotenv
 
 load_dotenv()   
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galery',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
